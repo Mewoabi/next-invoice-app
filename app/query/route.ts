318d2432ba551b6361bbb4392bc552eed05ca1df
doray <mewoabid@gmail.com>
2025-01-19 15:47:11 +0100

    almost done with course

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,4 +1,6 @@
-import { db } from "@vercel/postgres";
+import { db, sql } from "@vercel/postgres";
+import { LatestInvoice, LatestInvoiceRaw } from "../lib/definitions";
+import { formatCurrency } from "../lib/utils";
 
 const client = await db.connect();
 
@@ -13,13 +15,33 @@ async function listInvoices() {
 	return data.rows;
 }
 
+export async function fetchLatestInvoices() {
+  try {
+    const data = await client.sql<LatestInvoiceRaw>`
+      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
+      FROM invoices
+      JOIN customers ON invoices.customer_id = customers.id
+      ORDER BY invoices.date DESC
+      LIMIT 5`;
+
+    const latestInvoices = data.rows.map((invoice) => ({
+      ...invoice,
+      amount: formatCurrency(invoice.amount),
+    }));
+    return latestInvoices;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch the latest invoices.');
+  }
+}
+
 export async function GET() {
   // return Response.json({
   //   message:
   //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
   // });
   try {
-  	return Response.json(await listInvoices());
+  	return Response.json(await fetchLatestInvoices());
   } catch (error) {
   	return Response.json({ error }, { status: 500 });
   }
